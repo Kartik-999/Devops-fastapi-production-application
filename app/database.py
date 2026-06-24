47746747e2188838e2919e628fe937ff0305925b
kartik <kartiksadanala@gmail.com>
2026-06-24 21:22:10 +0530

    fixedd bugs so that fastapi runs

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:POSTGRES_PASSWORD@back_end:5432/studentdb")
+# DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:POSTGRES_PASSWORD@back_end:5432/studentdb")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
