bfe524633fb8ed2c3f9d7fd9b2bcc69e6aeda772
kartik <kartiksadanala@gmail.com>
2026-06-24 21:11:43 +0530

    removed hardedd db password and added git secret

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:12345@back_end:5432/studentdb")
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:POSTGRES_PASSWORD@back_end:5432/studentdb")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
