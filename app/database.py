671a97618cc550e659ee778fe3414b951509ce89
kartik <kartiksadanala@gmail.com>
2026-06-27 18:09:59 +0530

    modiffies docker compose so that postgres and redis starts first till then fastapi won't start

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 
-# DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:POSTGRES_PASSWORD@back_end:5432/studentdb")
+# DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:${POSTGRES_PASSWORD}@back_end:5432/studentdb")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
